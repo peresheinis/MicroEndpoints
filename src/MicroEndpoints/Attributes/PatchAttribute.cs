b3d69d110bbe4edc3be82a109931bc2788431e91
Илья <@DESKTOP-GR6LAPB>
2024-03-03 13:03:56 +0300

    created patch attribute

---
+++ src/MicroEndpoints/Attributes/PatchAttribute.cs
@@ -0,0 +1,19 @@
+﻿using MicroEndpoints.Interfaces;
+using Microsoft.AspNetCore.Builder;
+
+namespace MicroEndpoints.Attributes;
+
+public class PatchAttribute : Attribute, IHttpMethodAttribute
+{
+    public string Template { get; }
+
+    public PatchAttribute(string template)
+    {
+        Template = template;
+    }
+
+    public void ConfigureEndpoint(WebApplication app, Delegate requestDelegate)
+    {
+        app.MapPatch(Template, requestDelegate);
+    }
+}
